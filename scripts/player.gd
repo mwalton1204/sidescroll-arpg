5e03792517e9446a88fed599032efff7a9f7bcbf
Michael <rog41@txstate.edu>
2025-09-12 15:49:29 -0500

    feature(platforms): add platforms for the player to jump on

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,25 @@
 extends CharacterBody2D
 
-# -- Movement parameters --
 @export var speed : float = 200
 @export var jump_velocity : float = -400
 @export var gravity : float = 900
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Reset horizontal movement each frame
 	velocity.x = 0
 
-	# -- Handle input --
+	# Input
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= speed
-
-	# -- Apply gravity --
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# -- Jump --
+		
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = jump_velocity
 
-	# -- Move the character --
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Move the character
 	move_and_slide()
