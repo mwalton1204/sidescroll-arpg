a8427a7ba9c959675bacd33175ac44b7736e74b8
Michael Walton <waltonmichael.e@gmail.com>
2025-09-23 17:53:51 -0500

    Add rudimentary dash functionality

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,3 +1,14 @@
+"""
+TODO: Finish dash implementation
+	- Add dash_distance parameter
+	- Modify dash to use distance instead of time
+	- Allow dashing from standing still
+	- Review dash implementation to ensure that I understand it
+	- Allow air dashes toggleable in the inspector
+	- Move dash input to custom action
+	- Lerp velocity.y back into gravity instead of just instantly reapplying
+"""
+
 extends CharacterBody2D
 
 # --- MOVEMENT PARAMETERS ---
@@ -5,12 +16,15 @@ extends CharacterBody2D
 @export var speed: float = 200 ## Horizontal speed (pixels/sec)
 @export_range(0.1, 1.0, 0.01) var air_speed_multiplier: float = 0.75 ## Fraction of speed in the air
 @export var jump_height: float = 75.0 ## Jump height (pixels)
-@export var jump_duration: float = 0.5 ## Time to reach jump apex (sec)
+@export_range(0.0, 10, 0.1, "or_greater") var jump_duration: float = 0.5 ## Time to reach jump apex (sec)
 @export_range(0.0, 20.0, 1.0) var air_lerp_factor: float = 3.0  ## How fast horizontal speed transitions in the air
 @export_range(0.0, 0.25, 0.01) var coyote_time: float = 0.1 ## Time after leaving a platform that jump is still possible (sec)
 @export_range(0.0, 1.0, 0.01) var jump_buffer_time: float = 0.1  ## Time before landing to buffer jump input
 @export var max_jumps: int = 1 ## Maximum number of jumps (1 = single jump, 2 = double jump, etc.)
 @export var max_fall_speed: float = 1000.0 ## Maximum downward speed (pixels/sec)
+@export var dash_speed: float = 600.0 ## Dash speed (pixels/sec)
+@export var dash_duration: float = 0.2 ## Duration of dash (sec)
+@export var dash_cooldown: float = 1.0 ## Cooldown time between dashes (sec)
 
 # --- INTERNAL STATE ---
 
@@ -20,6 +34,10 @@ var speed_multiplier: float = 1.0 # Smoothed horizontal speed
 var coyote_timer: float = 0.0 # Timer for coyote time
 var jump_buffer_timer: float = 0.0 # Tracks remaining buffered jump time
 var jump_count: int = 0 # Tracks number of jumps used
+var dash_timer: float = 0.0 # Timer for dash duration
+var dash_cooldown_timer: float = 0.0 # Timer for dash cooldown
+var is_dashing: bool = false # Whether the player is currently dashing
+var dash_direction: int = 0 # -1 = left, 1 = right, 0 = none
 
 func _ready() -> void:
 	# Pre-calculate gravity and jump force for desired jump height/duration
@@ -33,6 +51,8 @@ func _physics_process(delta: float) -> void:
 	_apply_gravity(delta, on_floor)
 	_handle_horizontal_movement(delta, on_floor)
 	_handle_jump()
+	_handle_dash(delta, on_floor)
+
 	move_and_slide()
 
 # --- HELPER FUNCTIONS ---
@@ -85,3 +105,27 @@ func _perform_jump() -> void:
 	jump_buffer_timer = 0
 	jump_count += 1
 	coyote_timer = 0
+
+func _handle_dash(delta: float, on_floor: bool) -> void:
+	# Handle dash input, duration, and cooldown
+	dash_cooldown_timer = max(dash_cooldown_timer - delta, 0.0)
+
+	# Start dash if input is detected and not on cooldown
+	if Input.is_action_just_pressed("ui_down") and not is_dashing and dash_cooldown_timer <= 0:
+		var input_direction = Input.get_axis("ui_left", "ui_right")
+		if input_direction != 0:
+			is_dashing = true
+			dash_timer = dash_duration
+			dash_cooldown_timer = dash_cooldown
+			dash_direction = sign(input_direction)
+			velocity = Vector2(dash_direction * dash_speed, 0)
+			return
+
+	if is_dashing:
+		dash_timer -= delta
+		velocity.x = dash_direction * dash_speed
+		velocity.y = 0 # Neutralize vertical movement during dash
+
+		if dash_timer <= 0:
+			is_dashing = false
+			dash_direction = 0
